6d3b7f540eacd7118671d0f821a003c116b906d6
hyun9-9 <pwc1088@naver.com>
2024-01-19 10:58:40 +0900

    모델 : 댓글 전체 출력을 위한 질문댓글조회,유저댓글조회 및 댓글 생성 DAO추가

--- Balance_Game_Pro_V2/src/main/java/model/comment/CommentDAO.java
+++ Balance_Game_Pro_V2/src/main/java/model/comment/CommentDAO.java
@@ -9,43 +9,98 @@ import java.util.ArrayList;
 import model.Util.JDBCUtil;
 
 public class CommentDAO {
+	private Connection conn;
+	private PreparedStatement pstmt;
+	private static final String SELECTALL_Q = "SELECT C.CID,C.QID,C.LOGIN_ID,C.CONTENT,M.NAME\r\n"
+			+ "FROM COMMENTS C\r\n" + "LEFT OUTER JOIN MEMBER M ON C.LOGIN_ID =M.LOGIN_ID\r\n" + "WHERE C.QID=?";
+	private static final String SELECTALL_M = "SELECT C.CID,C.QID,C.LOGIN_ID,C.CONTENT,M.NAME\r\n"
+			+ "FROM COMMENTS C\r\n" + "LEFT OUTER JOIN MEMBER M ON C.LOGIN_ID =M.LOGIN_ID\r\n" + "WHERE C.LOGIN_ID=?";
+	private static final String INSERT = "INSERT INTO COMMENTS(CID,QID,LOGIN_ID,CONTENT)\r\n"
+			+ "VALUES ((SELECT NVL(MAX(CID),0) + 1 FROM COMMENTS),?,?,?)";
 
-    // 댓글 전체 출력하기 
-    public ArrayList<CommentDTO> selectAll(CommentDTO cDTO) {
-    	if(cDTO.getSearchCondition().equals("댓글조회")) {
-			//모델
-		}
-    	
-    	
-    	return null;
+	// 댓글 전체 출력하기
+	public ArrayList<CommentDTO> selectAll(CommentDTO cDTO) {
 
-    }
-    	
-    // 댓글 하나 출력하기 TODO 추후 구현 예정
-    public CommentDTO selectOne(CommentDTO cDTO) {
-    	return null;
-     }
-	
-    // 댓글 추가하기
-    public boolean insert(CommentDTO cDTO) {
-    	if(cDTO.getSearchCondition().equals("댓글생성")) {
-			//모델
+		ArrayList<CommentDTO> datas = new ArrayList<CommentDTO>();
+
+		conn = JDBCUtil.connect();
+		try {
+			if (cDTO.getSearchCondition().equals("질문댓글조회")) {// 질문에 대한
+				// 모델
+				pstmt = conn.prepareStatement(SELECTALL_Q);
+				pstmt.setInt(1, cDTO.getqId());
+			} else if (cDTO.getSearchCondition().equals("유저댓글조회")) {// 유저에 대한
+				// 모델
+				pstmt = conn.prepareStatement(SELECTALL_M);
+				pstmt.setString(1, cDTO.getLoginId());
+			}
+
+			ResultSet rs = pstmt.executeQuery();
+
+			while (rs.next()) {
+				CommentDTO data = new CommentDTO();
+				data.setcId(rs.getInt("CID"));
+				data.setqId(rs.getInt("QID"));
+				data.setLoginId(rs.getString("LOGIN_ID"));
+				data.setContent(rs.getString("CONTENT"));
+				data.setMemberName(rs.getString("NAME"));
+				datas.add(data);
+			}
+
+			rs.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			JDBCUtil.disconnect(pstmt, conn);
 		}
-    	
-      return false;
-    }
-    // 댓글 수정하기 TODO 추후 구현 예정
-    public boolean update(CommentDTO cDTO) {
-    	if(cDTO.getSearchCondition().equals("댓글수정")) {
-			//모델
+
+		return datas;
+
+	}
+
+	public CommentDTO selectOne(CommentDTO cDTO) {
+		return null;
+	}
+
+	// 댓글 추가하기
+	public boolean insert(CommentDTO cDTO) {
+//INSERT INTO COMMENTS(CID,QID,LOGIN_ID,CONTENT) VALUES ((SELECT NVL(MAX(CID),0) + 1 FROM COMMENTS),?,?,?)
+		conn = JDBCUtil.connect();
+		try {
+			if (cDTO.getSearchCondition().equals("댓글생성")) {
+				// 모델
+				pstmt = conn.prepareStatement(INSERT);
+				pstmt.setInt(1, cDTO.getqId());
+				pstmt.setString(2, cDTO.getLoginId());
+				pstmt.setString(3, cDTO.getContent());
+
+			}
+			int rs = pstmt.executeUpdate();
+			if (rs <= 0) {
+				return false;
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+			return false;
+		} finally {
+			JDBCUtil.disconnect(pstmt, conn);
 		}
-    	
-    	 return false;
-    }
-    // 댓글 삭제하기 TODO 추후 구현 예정
-    public boolean delete(CommentDTO cDTO) {
-    	//댓글 삭제
-    	 return false;
-    }
+		return true;
+	}
+
+	public boolean update(CommentDTO cDTO) {
+		if (cDTO.getSearchCondition().equals("댓글수정")) {
+			// 모델
+		} else if (cDTO.getSearchCondition().equals("회원탈퇴")) {
+			// 모델
+		}
+
+		return false;
+	}
+
+	public boolean delete(CommentDTO cDTO) {
+		// 댓글 삭제
+		return false;
+	}
 
 }
