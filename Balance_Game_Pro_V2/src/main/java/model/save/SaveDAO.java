231e2f1a5180a003dc18f43d5d6bbd5c10e8d237
hyun9-9 <pwc1088@naver.com>
2024-01-19 13:45:06 +0900

    모델 : 찜추가,찜삭제((질문PK+로그인아이디),(찜PK)) DAO 생성

--- Balance_Game_Pro_V2/src/main/java/model/save/SaveDAO.java
+++ Balance_Game_Pro_V2/src/main/java/model/save/SaveDAO.java
@@ -12,19 +12,26 @@ public class SaveDAO {
 
 	private Connection conn;
 	private PreparedStatement pstmt;
-	
-	private final String SELECTALL="SELECT Q.TITLE, Q.ANSWER_A, Q.ANSWER_B , Q.WRITER FROM SAVE S JOIN QUESTIONS Q ON S.QID = Q.QID WHERE S.LOGIN_ID = ?";
-	
+
+	private final String SELECTALL = "SELECT Q.TITLE, Q.ANSWER_A, Q.ANSWER_B , Q.WRITER FROM SAVE S JOIN QUESTIONS Q ON S.QID = Q.QID WHERE S.LOGIN_ID = ?";
+
+	private static final String INSERT = "INSERT INTO SAVE (SID, QID,LOGIN_ID) \r\n"
+			+ "VALUES((SELECT NVL(MAX(SID),0) + 1 FROM SAVE),?,?)";
+
+	private static final String DELETE_QID_LID = "DELETE FROM SAVE WHERE QID=? AND LOGIN_ID=?";
+
+	private static final String DELETE_SID = "DELETE FROM SAVE WHERE SID=?";
+
 	public ArrayList<SaveDTO> selectAll(SaveDTO sDTO) {
 
-		//전은주
+		// 전은주
 		conn = JDBCUtil.connect();
 		ArrayList<SaveDTO> datas = new ArrayList<SaveDTO>();
 		try {
 			pstmt = conn.prepareStatement(SELECTALL);
 			pstmt.setString(1, sDTO.getLoginId());
 			ResultSet rs = pstmt.executeQuery();
-			while(rs.next()) {
+			while (rs.next()) {
 				SaveDTO dto = new SaveDTO();
 				dto.setSaveTitle(rs.getString("TITLE"));
 				datas.add(dto);
@@ -43,7 +50,23 @@ public class SaveDAO {
 
 	public boolean insert(SaveDTO sDTO) {
 		// 찜하기
-		return false;
+		conn = JDBCUtil.connect();
+		try {
+			pstmt = conn.prepareStatement(INSERT);
+			pstmt.setInt(1, sDTO.getqId());
+			pstmt.setString(2, sDTO.getLoginId());
+			int rs = pstmt.executeUpdate();
+			if (rs <= 0) {
+				return false;
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+			return false;
+		} finally {
+			JDBCUtil.disconnect(pstmt, conn);
+		}
+		return true;
+
 	}
 
 	public boolean update(SaveDTO sDTO) {
@@ -51,13 +74,34 @@ public class SaveDAO {
 	}
 
 	public boolean delete(SaveDTO sDTO) {
-		if (sDTO.getSearchCondition().equals("qm찜삭제")) {
-			// 박현구
-		} else if (sDTO.getSearchCondition().equals("s찜삭제")) {
-			// 박현구
+		conn = JDBCUtil.connect();
+		try {
+			if (sDTO.getSearchCondition().equals("qm찜삭제")) {
+				// 박현구
+				pstmt=conn.prepareStatement(DELETE_QID_LID);
+				pstmt.setInt(1, sDTO.getqId());
+				pstmt.setString(2, sDTO.getLoginId());
+				int rs=pstmt.executeUpdate();
+				if(rs<=0) {
+					return false;
+				}
+				
+			} else if (sDTO.getSearchCondition().equals("s찜삭제")) {
+				// 박현구
+				pstmt=conn.prepareStatement(DELETE_SID);
+				pstmt.setInt(1, sDTO.getsId());
+				int rs=pstmt.executeUpdate();
+				if(rs<=0) {
+					return false;
+				}
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+			return false;
+		} finally {
+			JDBCUtil.disconnect(pstmt, conn);
 		}
-
-		return false;
+		return true;
 	}
 
 }
