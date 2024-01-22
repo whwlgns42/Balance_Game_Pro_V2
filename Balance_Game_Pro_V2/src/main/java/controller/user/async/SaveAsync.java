40ed04ec5e76b5a0d920fa24c2ea9cdb31d7741c
hyun9-9 <pwc1088@naver.com>
2024-01-22 16:21:31 +0900

    webServlet 수정

--- Balance_Game_Pro_V2/src/main/java/controller/user/async/SaveAsync.java
+++ Balance_Game_Pro_V2/src/main/java/controller/user/async/SaveAsync.java
@@ -12,7 +12,7 @@ import javax.servlet.http.HttpServletResponse;
 import model.save.SaveDAO;
 import model.save.SaveDTO;
 
-@WebServlet("/SaveAsync.do")
+@WebServlet("/saveAsync.do")
 public class SaveAsync extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
