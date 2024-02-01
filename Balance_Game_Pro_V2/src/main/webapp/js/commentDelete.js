0fc1f4afb63c18080a72bce31452d6b0900cc9a5
DESKTOP-HUJUVSS\Administrator <mhdg123@naver.com>
2024-02-01 17:00:08 +0900

    삭제 alert창 작성

--- Balance_Game_Pro_V2/src/main/webapp/js/commentDelete.js
+++ Balance_Game_Pro_V2/src/main/webapp/js/commentDelete.js
@@ -1,26 +1,30 @@
-/**
- * 
- */
-console.log("[생성]");
-$("#commentDelete").on("click",function(){
-	console.log("[댓글삭제성공]");
-	var cId = document.getElementById('cId').value;
-	$.ajax({
-		type: "POST",
-		url: "DeleteCommentAsync.do",
-		data: {'cId': cId},
-		dataType: 'text',
-		success: function(data){
-			console.log(data);
-			if(data == '0'){
-				alert('삭제에 실패했습니다');
-			}else{
-				alert('삭제했습니다');
-				location.reload();
-			}
-		},
-		error(error){
-			console.log("error"+error);
-		}
-	});
+$("#commentDelete").on("click", function() {
+    console.log("[댓글삭제성공]");
+    var cId = document.getElementById('cId').value;
+    $.ajax({
+        type: "POST",
+        url: "DeleteCommentAsync.do",
+        data: { 'cId': cId },
+        dataType: 'text',
+        success: function(data) {
+            console.log(data);
+            if (data == '0') {
+                alert('삭제에 실패했습니다');
+            } else {
+                Swal.fire({
+                    title: "성공",
+                    text: "삭제했습니다",
+                    icon: "success"
+                }).then((result) => {
+                    if (result.isConfirmed) {
+                        // 확인 버튼이 눌렸을 때 실행할 코드
+                        location.reload();
+                    }
+                });
+            }
+        },
+        error: function(error) {
+            console.log("error" + error);
+        }
+    });
 });
