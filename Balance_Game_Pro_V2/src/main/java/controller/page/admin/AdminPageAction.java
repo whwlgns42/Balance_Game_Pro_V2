c7c55879eb7a9a98e00db2aabfc56a8484646754
DESKTOP-HUJUVSS\Administrator <mhdg123@naver.com>
2024-02-01 13:39:23 +0900

    관리자메인페이지에서 사용할 데이터 작성

--- Balance_Game_Pro_V2/src/main/java/controller/page/admin/AdminPageAction.java
+++ Balance_Game_Pro_V2/src/main/java/controller/page/admin/AdminPageAction.java
@@ -9,8 +9,14 @@ import javax.servlet.http.HttpServletResponse;
 
 import controller.common.Action;
 import controller.common.ActionForward;
+import model.member.MemberDAO;
+import model.member.MemberDTO;
+import model.question.QuestionDAO;
+import model.question.QuestionDTO;
 import model.suggestion.SuggestionDAO;
 import model.suggestion.SuggestionDTO;
+import model.support.SupportDAO;
+import model.support.SupportDTO;
 
 public class AdminPageAction implements Action {
 
@@ -18,14 +24,32 @@ public class AdminPageAction implements Action {
 	public ActionForward execute(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		ActionForward forward = new ActionForward();
-		
+
 		SuggestionDTO sugDTO = new SuggestionDTO();
 		SuggestionDAO sugDAO = new SuggestionDAO();
+		QuestionDTO qDTO = new QuestionDTO();
+		QuestionDAO qDAO = new QuestionDAO();
+		MemberDTO mDTO = new MemberDTO();
+		MemberDAO mDAO = new MemberDAO();
+		SupportDTO sDTO = new SupportDTO();
+		SupportDAO sDAO = new SupportDAO();
+		
+		qDTO.setSearchCondition("문제승인갯수");
+		QuestionDTO qDTOApproveCnt = qDAO.selectOne(qDTO);
+		
+		qDTO.setSearchCondition("총문제수");
+		QuestionDTO qDTOTotalCnt = qDAO.selectOne(qDTO);
+
+		mDTO.setSearchCondition("회원인원수");
+		mDTO = mDAO.selectOne(mDTO);
+		
+		sDTO.setSearchCondition("총후원금액");
+		sDTO = sDAO.selectOne(sDTO);
 		
 		ArrayList<SuggestionDTO> sugDatas = sugDAO.selectAll(sugDTO);
-		System.out.println(sugDatas+ "sugDatas <<<<<<<<");
-		
-		if(sugDatas.isEmpty()) {
+		System.out.println(sugDatas + "sugDatas <<<<<<<<");
+
+		if (sugDatas.isEmpty()) {
 			sugDatas = null;
 			System.out.println("건의사항 널 뜬다");
 		}
@@ -33,6 +57,11 @@ public class AdminPageAction implements Action {
 		forward.setPath("adminMain.jsp");
 		forward.setRedirect(false);
 		request.setAttribute("sugDatas", sugDatas);
+		request.setAttribute("qDTOApproveCnt", qDTOApproveCnt);
+		request.setAttribute("qDTOTotalCnt", qDTOTotalCnt);
+		request.setAttribute("mDTO", mDTO);
+		request.setAttribute("sDTO", sDTO);
+	
 		return forward;
 	}
 
