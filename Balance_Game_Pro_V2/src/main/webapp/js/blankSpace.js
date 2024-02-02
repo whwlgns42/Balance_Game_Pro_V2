7b0019d9d6c2e06f8b4ad09f2278e5f2ec82f5db
hyun9-9 <pwc1088@naver.com>
2024-02-02 11:27:23 +0900

    Revert "디테일 수정"
    This reverts commit 23cd6ef3df5d24e9fc6db84498b4bab759042bd6.

--- Balance_Game_Pro_V2/src/main/webapp/js/blankSpace.js
+++ Balance_Game_Pro_V2/src/main/webapp/js/blankSpace.js
@@ -2,17 +2,15 @@
  * 
  */
 function blankSpace() {
-    var flag = true;
-    // 모든 input 요소의 value 확인하기
-    $('input[type="text"]').each(function() {
-        var content = $(this).val().trim(); // trim() 함수를 호출합니다.
-        //console.log("Input Value: " + content);
-        $(this).val(content);
-        if (content === '') { // 입력 값이 공백이면
-            flag = false;
-            return false; // 반복문을 종료합니다.
-        }
-        
-    });
-    return flag;
+	var flag = true;
+	// 모든 input 요소의 value 확인하기
+	$('input[type="text"]').each(function() {
+		var content = $(this).val();
+		console.log("Input Value: " + content);
+		if ($.trim(content) === '') { // 입력 값이 공백이면
+			flag = false;
+			return false; // 반복문을 종료합니다.
+		}
+	});
+	return flag;
 }
