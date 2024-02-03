4c73f0fc10eee85aa476dd1473fd2c5b64e44a77
hyun9-9 <pwc1088@naver.com>
2024-02-03 12:59:05 +0900

    댓글 전체 조회시 탈퇴한 사용자 모델에서 처리로 인한 .js 수정

--- Balance_Game_Pro_V2/src/main/webapp/js/commentAll.js
+++ Balance_Game_Pro_V2/src/main/webapp/js/commentAll.js
@@ -2,9 +2,9 @@
  * 
  */
 
-		
+
 function commentAll(qId) {
-	
+
 
 
 	$.ajax({
@@ -19,20 +19,18 @@ function commentAll(qId) {
 			var elem = "";
 			$.each(data, function(index, data) {
 				elem += "<tr> <td>";
-				if (typeof data.loginId != "undefined") {
-					if (data.grade == 1) {
-						elem += "<img src='images/blackStone.png' alt='등급1에 주는 블랙스톤' width='25' height ='25' />";
-					} else if (data.grade == 2) {
-						elem += "<img src='images/silverStone.png' alt='등급2에 주는 실버스톤' width='25' height ='25' />";
-					} else if (data.grade == 3) {
-						elem += "<img src='images/goldStone.png' alt='등급3에 주는 골드스톤' width='25' height ='25' />";
-					} else if (data.grade == 4) {
-						elem += "<img src='images/blueStone.png' alt='등급4에 주는 플래티넘스톤' width='25' height ='25' />";
-					}
-					elem += data.memberName + "( " + data.loginId + " )</td>";
-				} else {
-					elem += "탈퇴한 사용자</td>"
+
+				if (data.grade == 1) {
+					elem += "<img src='images/blackStone.png' alt='등급1에 주는 블랙스톤' width='25' height ='25' />";
+				} else if (data.grade == 2) {
+					elem += "<img src='images/silverStone.png' alt='등급2에 주는 실버스톤' width='25' height ='25' />";
+				} else if (data.grade == 3) {
+					elem += "<img src='images/goldStone.png' alt='등급3에 주는 골드스톤' width='25' height ='25' />";
+				} else if (data.grade == 4) {
+					elem += "<img src='images/blueStone.png' alt='등급4에 주는 플래티넘스톤' width='25' height ='25' />";
 				}
+				elem += data.memberName + "</td>";
+
 				elem += "<td>" + data.content + "</td>";
 				elem += "</tr>";
 				console.log(data.name);
