0ce78585f44c8bec6fda8a8172c04bf3f421ebc6
hyun9-9 <pwc1088@naver.com>
2024-02-05 15:25:00 +0900

    < , > 입력불가

--- Balance_Game_Pro_V2/src/main/webapp/js/blankSpace.js
+++ Balance_Game_Pro_V2/src/main/webapp/js/blankSpace.js
@@ -2,24 +2,21 @@
  * 
  */
 function blankSpace() {
-	var flag = true;
-	// 모든 input 요소의 value 확인하기
-	$('input[type="text"]').each(function() {
-		var content = $(this).val().trim(); // trim() 함수를 호출합니다.
-		//console.log("Input Value: " + content);
+    var flag = true;
+    // 모든 input 요소의 value 확인하기
+    $('input[type="text"]').each(function() {
+        var content = $(this).val().trim(); // trim() 함수를 호출합니다.
+        // < 나 > 가 하나라도 입력되었는지 확인
+        if (content.includes('<') || content.includes('>')) {
+			console.log('< 입력');
+            flag = false;     
+        }
 
-		var regExp = /\<\>/gi;
-		if (regExp.test(content)) {
-			flag = false;
-		}
+        $(this).val(content);
+        if (content === '') { // 입력 값이 공백이면
+            flag = false;
+        }
 
-
-		$(this).val(content);
-		if (content === '') { // 입력 값이 공백이면
-			flag = false;
-			//return false; // 반복문을 종료합니다.
-		}
-
-	});
-	return flag;
+    });
+    return flag;
 }
