549222e22de3e982363dc9fbf4a3da696f7f93df
hyun9-9 <pwc1088@naver.com>
2024-02-05 15:11:32 +0900

    후원랭킹 - 탈퇴한 사용자끼리 더해짐(탈퇴한 사용자는 안띄우게 수정)

--- Balance_Game_Pro_V2/src/main/java/model/support/SupportDAO.java
+++ Balance_Game_Pro_V2/src/main/java/model/support/SupportDAO.java
@@ -18,15 +18,17 @@ public class SupportDAO {
 	private static final String SELECTALL = "SELECT S.LOGIN_ID, SUM(S.AMOUNT) \"TOTAL\", M.NAME FROM SUPPORT S JOIN MEMBER M ON S.LOGIN_ID = M.LOGIN_ID GROUP BY S.LOGIN_ID, M.NAME ORDER BY total DESC";
 
 	private static final String SELECTALL_RANKING = "SELECT \r\n"
-			+ "    NVL(S.LOGIN_ID, '탈퇴한 사용자') AS LOGIN_ID,\r\n"
+			+ "    S.LOGIN_ID,\r\n"
 			+ "    SUM(S.AMOUNT) AS TOTAL,\r\n"
-			+ "    NVL(M.NAME, '탈퇴한 사용자') AS NAME,\r\n"
+			+ "    M.NAME,\r\n"
 			+ "    RANK() OVER (ORDER BY SUM(S.AMOUNT) DESC, MIN(S.REG_DATE)) AS RANKING \r\n"
 			+ "FROM \r\n"
 			+ "    SUPPORT S \r\n"
 			+ "LEFT OUTER JOIN \r\n"
 			+ "    MEMBER M ON S.LOGIN_ID = M.LOGIN_ID \r\n"
-			+ "GROUP BY \r\n"
+			+ "WHERE \r\n"
+			+ "    S.LOGIN_ID IS NOT NULL\r\n"
+			+ "GROUP BY  \r\n"
 			+ "    S.LOGIN_ID, M.NAME";
 
 	// 회원탈퇴시 'Support'을 null 값으로 변경
