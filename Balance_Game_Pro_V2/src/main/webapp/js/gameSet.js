1d15fd3aaa0ff593887bc8efc80bdbabe8045d17
hyun9-9 <pwc1088@naver.com>
2024-02-06 13:53:24 +0900

    새로고침 수정

--- Balance_Game_Pro_V2/src/main/webapp/js/gameSet.js
+++ Balance_Game_Pro_V2/src/main/webapp/js/gameSet.js
@@ -22,7 +22,7 @@ $(document).ready(function() {
 
 	});
 
-	$(document).keydown(function(e) {
+/*	$(document).keydown(function(e) {
 		key = (e) ? e.keyCode : e.keyCode;
 		
 		var t = document.activeElement;
@@ -44,5 +44,30 @@ $(document).ready(function() {
 				} else { e.keyCode = 0; e.returnValue = false; }
 			}
 		}
-	});
+	});*/
+	
+	
+	
+$(document).keydown(function(event) {
+    var key = event.keyCode || event.which;
+    var targetTagName = event.target.tagName.toLowerCase();
+
+    // Backspace 키 누르면서 입력창이 아닌 경우에는 뒤로가기를 막습니다.
+    if (key === 8 && targetTagName !== "input" && targetTagName !== "textarea") {
+        event.preventDefault();
+    }
+
+    // F5 키와 Ctrl+R 키를 누르면 새로고침을 막습니다.
+    if (key === 116 || (event.ctrlKey && key === 82)) {
+        event.preventDefault();
+       	Swal.fire({
+			title: "새로고침",
+			text: "새로고침이 금지되어 있습니다.",
+			icon: "info"
+		});
+        
+        //alert("새로고침이 금지되어 있습니다.");
+    }
+});
+	
 });
